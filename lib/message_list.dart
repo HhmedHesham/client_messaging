36a9a4ecdc1d088bdb7c3f7be89b05e745a28628
HhmedHesham <heshama7793@gmail.com>
2022-05-10 09:40:22 +0200

    implementing slidable to remove un important messages

--- lib/message_list.dart
+++ lib/message_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:message_client/compose_button.dart';
 import 'package:message_client/message_details.dart';
 
@@ -18,26 +20,55 @@ class _MessageListState extends State<MessageList> {
   @override
   void initState() {
     messages = Message.loadMesageList();
-    // TODO: implement initState
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title!),
-        leading: IconButton(
+      drawer: Drawer(
+        child: Column(
+          children: const [
+            UserAccountsDrawerHeader(
+              accountName: Text('Ahmed Hesham'),
+              accountEmail: Text('heshama793@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://avatars.githubusercontent.com/u/57070673?v=4'),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                FontAwesomeIcons.inbox,
+              ),
+              title: Text('Inbox'),
+              trailing: Chip(
+                label: Text('10'),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+              ),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(title: Text(widget.title!), actions: [
+        IconButton(
           icon: const Icon(
             Icons.refresh,
           ),
           onPressed: () {
-            setState(() {
-              messages = Message.loadMesageList();
-            });
+            setState(
+              () {
+                messages = Message.loadMesageList();
+              },
+            );
           },
         ),
-      ),
+      ]),
       body: FutureBuilder(
           future: messages,
           builder: (context, snapshot) {
@@ -59,28 +90,92 @@ class _MessageListState extends State<MessageList> {
                 List<Message> _messages = snapshot.data as List<Message>;
                 return ListView.separated(
                   itemBuilder: (context, index) {
+                    // create golbal keys for each list-tile
+                    // GlobalKey _key = GlobalKey();
                     Message message = _messages[index];
-                    return ListTile(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (_) => MessageDetails(
-                              body: message.body!,
-                              title: message.subject!,
-                            ),
+                    return Slidable(
+                      key: UniqueKey(),
+                      // The start action pane is the one at the left or the top side.
+                      startActionPane: ActionPane(
+                        // A motion is a widget used to control how the pane animates.
+                        motion: const ScrollMotion(),
+
+                        // A pane can dismiss the Slidable.
+                        dismissible: DismissiblePane(onDismissed: () {
+                          print('message removed');
+                          setState(() {
+                            _messages.removeAt(index);
+                          });
+                        }),
+
+                        // All actions are defined in the children parameter.
+                        children: [
+                          // A SlidableAction can have an icon and/or a label.
+                          SlidableAction(
+                            onPressed: (_) {
+                              print('message removed');
+                              setState(() {
+                                _messages.removeAt(index);
+                              });
+                            },
+                            backgroundColor: const Color(0xFFFE4A49),
+                            foregroundColor: Colors.white,
+                            icon: Icons.delete,
+                            label: 'Delete',
                           ),
-                        );
-                      },
-                      isThreeLine: true,
-                      leading: const CircleAvatar(
-                        backgroundColor: Colors.amber,
-                        child: Text('AH'),
+                          SlidableAction(
+                            onPressed: (_) {},
+                            backgroundColor: const Color(0xFF21B7CA),
+                            foregroundColor: Colors.white,
+                            icon: Icons.share,
+                            label: 'Share',
+                          ),
+                        ],
                       ),
-                      title: Text(message.subject!),
-                      subtitle: Text(
-                        message.body!,
-                        overflow: TextOverflow.ellipsis,
+                      // The end action pane is the one at the right or the bottom side.
+                      endActionPane: ActionPane(
+                        motion: const ScrollMotion(),
+                        children: [
+                          SlidableAction(
+                            // An action can be bigger than the others.
+                            flex: 2,
+                            onPressed: (_) {},
+                            backgroundColor: const Color(0xFF7BC043),
+                            foregroundColor: Colors.white,
+                            icon: Icons.archive,
+                            label: 'Archive',
+                          ),
+                          SlidableAction(
+                            onPressed: (_) {},
+                            backgroundColor: const Color(0xFF0392CF),
+                            foregroundColor: Colors.white,
+                            icon: Icons.save,
+                            label: 'Save',
+                          ),
+                        ],
+                      ),
+                      child: ListTile(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => MessageDetails(
+                                body: message.body!,
+                                title: message.subject!,
+                              ),
+                            ),
+                          );
+                        },
+                        isThreeLine: true,
+                        leading: const CircleAvatar(
+                          backgroundColor: Colors.amber,
+                          child: Text('AH'),
+                        ),
+                        title: Text(message.subject!),
+                        subtitle: Text(
+                          message.body!,
+                          overflow: TextOverflow.ellipsis,
+                        ),
                       ),
                     );
                   },
